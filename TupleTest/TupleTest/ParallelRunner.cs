6fd9e3f89c2b2c326540e8c0b132979144b50bae
Dániel Molnár <danielmolnar@hotmail.hu>
2020-04-03 22:42:07 +0200

    Init

---
+++ TupleTest/TupleTest/ParallelRunner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace TupleTest
+{
+    public static class ParallelRunner
+    {
+        public static void Run(Action<Tuple<int, int>> act, int collectionSize)
+        {
+            var rangePartitioner = Partitioner.Create(0, collectionSize, collectionSize / MaxCoreCount + 1);
+            Parallel.ForEach(rangePartitioner, (range, loopState) =>
+            {
+                act.Invoke(range);
+            });
+        }
+
+        public static void Run(Calculator act, int collectionSize)
+        {
+            var rangePartitioner = Partitioner.Create(0, collectionSize, collectionSize / MaxCoreCount + 1);
+            Parallel.ForEach(rangePartitioner, (range, loopState) =>
+            {
+                act(range).Invoke();
+            });
+        }
+
+        public delegate Action Calculator(Tuple<int, int> range);
+
+        public static readonly int MaxCoreCount = Environment.ProcessorCount;
+    }
+}
---
+++ TupleTest/TupleTest/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace TupleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                RunDelegate();
+                RunTuple();
+            }
+        }
+
+        private static void RunDelegate()
+        {
+            var arrayDelegate = new double[1000000];
+
+            Action DelegateCalc(Tuple<int, int> range)
+            {
+                return () =>
+                {
+                    for (int i = range.Item1; i < range.Item2; i++)
+                    {
+                        arrayDelegate[i] = Math.Pow(i, 2);
+                    }
+                };
+            }
+
+            myStopwatch.Restart();
+
+            ParallelRunner.Run(DelegateCalc, arrayDelegate.Length);
+
+            File.AppendAllText("log.txt", "Delegate: " + myStopwatch.ElapsedTicks + Environment.NewLine);
+        }
+
+        private static void RunTuple()
+        {
+            var arrayTuple = new double[1000000];
+
+            myStopwatch.Restart();
+
+            void TupleCalc(Tuple<int, int> range)
+            {
+                for (int i = range.Item1; i < range.Item2; i++)
+                {
+                    arrayTuple[i] = Math.Pow(i, 2);
+                }
+            }
+
+            ParallelRunner.Run(TupleCalc, arrayTuple.Length);
+
+            File.AppendAllText("log.txt", "Tuple: " + myStopwatch.ElapsedTicks + Environment.NewLine);
+        }
+
+        private static readonly Stopwatch myStopwatch = new Stopwatch();
+    }
+}
